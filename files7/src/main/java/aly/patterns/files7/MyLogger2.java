2a7c78a28fa3e50cf829041d48d32ba0ce3e1b28
aly <alexey.yakubovich@searshc.com>
2015-03-06 13:12:36 -0600

    First

---
+++ files7/src/main/java/aly/patterns/files7/MyLogger2.java
@@ -0,0 +1,47 @@
+package aly.patterns.files7;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public class MyLogger2
+{
+	static public void main(String [] args)
+	{
+		Logger logger = createMyLogger(null, Level.DEBUG);   //ALL, INFO, ...
+		logger.info("info");
+		logger.debug("debug");
+		logger.warn("warn");
+		logger.fatal("fatal");
+	}
+	
+	private static String LOGFILE = "/Users/Proj/LunaJD/aly.kafka.play/Data/myLog.log";
+	
+	public static Logger createMyLogger(String filePath, Level aThreshold)				// can be null
+	{
+		String logFile = (filePath == null ? LOGFILE : filePath);		// we don't check that file exists
+		Level threshold = (aThreshold == null ? Level.INFO : aThreshold);
+		
+		Logger myLogger = Logger.getLogger(MyLogger2.class);
+		FileAppender fileAppender = new FileAppender();
+		fileAppender.setName("MyFileLogger");
+		fileAppender.setFile(logFile);
+		fileAppender.setLayout(new PatternLayout("%d %-5p [%c{1}] %m%n"));
+		fileAppender.setThreshold(threshold);
+		fileAppender.setAppend(true);
+		fileAppender.activateOptions();
+		
+		ConsoleAppender conAppender = new ConsoleAppender();
+		conAppender.setName("MyConsoleAppender");
+		conAppender.setLayout(new PatternLayout("%d %-5p [%c{1}] %m%n"));
+		conAppender.setThreshold(threshold);
+		conAppender.activateOptions();
+		
+		myLogger.setAdditivity(false);
+		myLogger.addAppender(fileAppender);
+		myLogger.addAppender(conAppender);
+		return myLogger;
+	}
+}
